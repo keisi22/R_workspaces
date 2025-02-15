9003aee04622ae8f13f812206971995908bf3ef9
keisi22 <keisi22newnew@gmail.com>
2025-02-15 16:45:51 +0900

    Add files via upload

--- 0215/20250215_연습문제.R
+++ 0215/20250215_연습문제.R
@@ -42,6 +42,21 @@ print(colnames(people))
 # 힌트: data[row_condition, column_condition] 콤마를 기준으로 왼쪽은 행 조건, 오른쪽은 열 조건입니다. 
 #result=people[]
 
-데이터 마지막 3개행을 출력하기
+# 데이터 마지막 3개행을 출력하기
 # 힌트: tail
-print(tail)
+print(tail(people,3))
+
+# 새로운 컬럼 'weight' 추가, 데이터는 52,77,81,46 차례로 넣어주세요.
+# 힌트: people$'새로운컬럼명' = '값'
+people$weight=c(52, 77,81,46)
+print(people)
+
+# 나이가 30 이상인 사람들만 출력하는 코드를 작성하세요.
+# 힌트: 데이터프레임 필터링은 대괄호 [ ]를 사용합니다.
+result=people[people$age >=30,]
+print(result)
+
+# name , age 두개의 컬럼으로 구성된 새로운 데이터 프레임을 정의하기.
+# 힌트 : [,c('','')]
+result=people[, c('name','age')]
+print(result)
